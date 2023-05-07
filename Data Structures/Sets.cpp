9c66ebbbe7c4903c4721c3d58f0ec72ddc072a91
Satyen Subramaniam <66653384+SubramaniamSatyen@users.noreply.github.com>
2023-05-06 17:04:02 -0700

    Sets

---
+++ Data Structures/Sets.cpp
@@ -0,0 +1,29 @@
+//#include <set>
+//#include <iostream>
+//using namespace std;
+//
+//
+//int main() {
+//	set<int> temp;
+//
+//	for (int i = 10; i > 0; i--) {
+//		temp.insert(i);
+//	}
+//
+//	const set<int>::iterator firstVal = temp.begin();
+//
+//
+//	auto iter = temp.begin();
+//
+//	while (iter != temp.end()) {
+//		cout << *iter << endl;
+//		iter++;
+//	}
+//
+//	//int* ptr = new int(5);
+//	//cout << *ptr << endl;
+//
+//
+//	cout << (temp.find(5) != temp.end()) << endl;
+//	cout << (temp.find(11)!= temp.end()) << endl;
+//}
