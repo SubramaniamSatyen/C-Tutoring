eb6b6fbd331be6a9a5b211be9767999a30d7e4cb
Satyen Subramaniam <66653384+SubramaniamSatyen@users.noreply.github.com>
2023-05-07 17:05:36 -0700

    Update Sets.cpp

--- Data Structures/Sets.cpp
+++ Data Structures/Sets.cpp
@@ -1,29 +1,48 @@
-//#include <set>
-//#include <iostream>
-//using namespace std;
-//
-//
-//int main() {
-//	set<int> temp;
-//
-//	for (int i = 10; i > 0; i--) {
-//		temp.insert(i);
-//	}
-//
-//	const set<int>::iterator firstVal = temp.begin();
-//
-//
-//	auto iter = temp.begin();
-//
-//	while (iter != temp.end()) {
-//		cout << *iter << endl;
-//		iter++;
-//	}
-//
-//	//int* ptr = new int(5);
-//	//cout << *ptr << endl;
-//
-//
-//	cout << (temp.find(5) != temp.end()) << endl;
-//	cout << (temp.find(11)!= temp.end()) << endl;
-//}
+#include <set>
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	set<int> temp;
+
+	for (int i = 10; i > 0; i--) {
+		temp.insert(i);
+	}
+
+	const set<int>::iterator firstVal = temp.begin();
+
+
+	auto iter = temp.begin();
+
+	while (iter != temp.end()) {
+		cout << *iter << endl;
+		iter++;
+	}
+
+	//int* ptr = new int(5);
+	//cout << *ptr << endl;
+
+
+	cout << (temp.find(5) != temp.end()) << endl;
+	cout << (temp.find(11)!= temp.end()) << endl;
+
+
+	set<int> a({ 1, 2, 3, 4, 5 });
+	set<int> b = a;
+	b.insert(6);
+	cout << a.size() << endl; 
+	cout << b.size() << endl;
+
+
+	set<int>* c = &a;
+	c->insert(6);
+	cout << a.size() << endl;
+	cout << b.size() << endl;
+	cout << c->size() << endl;
+
+	int intone = 1;
+	int inttwo = 2;
+	set<int*> ptrA({ &intone, &inttwo });
+	set<int*> ptrB = ptrA;
+}
